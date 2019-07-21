56505d8756c8f1f732acc14fcc5cc7bbc8d4e8a1
Sergio Fidanli <gfidanli@gmail.com>
2019-07-21 13:16:15 -0700

    create initial db schema

---
+++ schema.sql
@@ -0,0 +1,59 @@
+-- Define Database Schema
+
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS titles;
+DROP TABLE IF EXISTS department_employee;
+DROP TABLE IF EXISTS department_manager;
+
+
+-- Create a table of departments
+CREATE TABLE departments (
+  ID VARCHAR PRIMARY KEY,
+  dept_name VARCHAR NOT NULL,
+);
+
+-- Create a table of employees
+CREATE TABLE employees (
+    ID INT PRIMARY KEY,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    gender VARCHAR(1) NOT NULL,
+    hire_date DATE NOT NULL
+);
+
+-- Create a table of salaries
+CREATE TABLE salaries (
+    ID INT PRIMARY KEY,
+    FOREIGN KEY (employee_ID) REFERENCES employees(ID),
+    salary INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE
+);
+
+-- Create a table of titles
+CREATE TABLE titles (
+    ID INT PRIMARY KEY,
+    FOREIGN KEY (employee_ID) REFERENCES employees(ID),
+    title VARCHAR(30) NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE
+);
+
+-- Create a junction table for Departments/Employees
+CREATE TABLE department_employee (
+    ID INT PK,
+    FOREIGN KEY (department_ID) REFERENCES departments(ID),
+    FOREIGN KEY (employee_ID) REFERENCES employees(ID),
+    from_date DATE NOT NULL,
+    to_date DATE
+);
+
+-- Create a junction table for Departments/Manager
+CREATE TABLE department_manager (
+    ID INT PK,
+    FOREIGN KEY (department_ID) REFERENCES departments(ID),
+    FOREIGN KEY (employee_ID) REFERENCES employees(ID),
+);
