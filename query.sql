6193fc5efaecea4456493c5b7e7ad835a6e29590
Sergio Fidanli <gfidanli@gmail.com>
2019-07-23 06:56:40 -0700

    add queries 4-5

--- query.sql
+++ query.sql
@@ -15,9 +15,24 @@ WHERE hire_date >= '1986-01-01'
 -- department number, department name, the manager's employee number, 
 -- last name, first name, and start and end employment dates.
 
-SELECT d.ID, d.dept_name, e.ID, e.last_name, e.first_name, de.from_date, de.to_date
+SELECT d.ID, d.dept_name, e.ID, e.last_name, e.first_name, dm.from_date, dm.to_date
 FROM employees AS e
-LEFT JOIN department_employee as de ON e.ID = de.emp_no
-LEFT JOIN departments as d ON d.ID = de.dept_no;
+INNER JOIN department_manager as dm ON e.ID = dm.emp_no
+INNER JOIN departments as d ON d.ID = dm.dept_no;
+
+-- 4. List the department of each employee with the following information: 
+-- employee number, last name, first name, and department name.
+
+SELECT e.ID, e.last_name, e.first_name, d.dept_name, de.from_date, de.to_date
+FROM employees AS e
+INNER JOIN department_employee AS de ON e.ID = de.emp_no
+INNER JOIN departments as d on d.ID = de.dept_no;
+
+-- 5. List all employees whose first name is "Hercules" and last names begin with "B."
+
+SELECT *
+FROM EMPLOYEES
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
 
 
